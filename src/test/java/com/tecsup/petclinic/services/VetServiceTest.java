b88e039763ef839fafdbeb27a1780f69aee3322a
Jilder Dionisio <jilder.dionisio@tecsup.edu.pe>
2026-05-26 14:33:02 -0500

    test: agregar pruebas de actualización y eliminación de veterinarios

--- src/test/java/com/tecsup/petclinic/services/VetServiceTest.java
+++ src/test/java/com/tecsup/petclinic/services/VetServiceTest.java
@@ -81,4 +81,91 @@ public class VetServiceTest {
         assertEquals(ACTIVE, newVetDTO.getActive());
     }
 
+    @Test
+    public void testUpdateVet() {
+
+        String FIRST_NAME = "Mark";
+        String LAST_NAME = "Brown";
+        String EMAIL = "mark.brown@petclinic.com";
+        String PHONE = "6085558888";
+        Boolean ACTIVE = true;
+
+        String UP_FIRST_NAME = "Markus";
+        String UP_LAST_NAME = "Brownson";
+        String UP_EMAIL = "markus.brownson@petclinic.com";
+        String UP_PHONE = "6085559999";
+        Boolean UP_ACTIVE = false;
+
+        VetDTO vetDTO = VetDTO.builder()
+                .firstName(FIRST_NAME)
+                .lastName(LAST_NAME)
+                .email(EMAIL)
+                .phone(PHONE)
+                .active(ACTIVE)
+                .build();
+
+        // ------------ Create ---------------
+
+        log.info(">" + vetDTO);
+        VetDTO vetDTOCreated = this.vetService.create(vetDTO);
+        log.info(">>" + vetDTOCreated);
+
+        // ------------ Update ---------------
+
+        vetDTOCreated.setFirstName(UP_FIRST_NAME);
+        vetDTOCreated.setLastName(UP_LAST_NAME);
+        vetDTOCreated.setEmail(UP_EMAIL);
+        vetDTOCreated.setPhone(UP_PHONE);
+        vetDTOCreated.setActive(UP_ACTIVE);
+
+        VetDTO upgradeVetDTO = this.vetService.update(vetDTOCreated);
+        log.info(">>>>" + upgradeVetDTO);
+
+        assertEquals(UP_FIRST_NAME, upgradeVetDTO.getFirstName());
+        assertEquals(UP_LAST_NAME, upgradeVetDTO.getLastName());
+        assertEquals(UP_EMAIL, upgradeVetDTO.getEmail());
+        assertEquals(UP_PHONE, upgradeVetDTO.getPhone());
+        assertEquals(UP_ACTIVE, upgradeVetDTO.getActive());
+    }
+
+    @Test
+    public void testDeleteVet() {
+
+        String FIRST_NAME = "Delete";
+        String LAST_NAME = "Vet";
+        String EMAIL = "delete.vet@petclinic.com";
+        String PHONE = "6085550000";
+        Boolean ACTIVE = true;
+
+        // ------------ Create ---------------
+
+        VetDTO vetDTO = VetDTO.builder()
+                .firstName(FIRST_NAME)
+                .lastName(LAST_NAME)
+                .email(EMAIL)
+                .phone(PHONE)
+                .active(ACTIVE)
+                .build();
+
+        VetDTO newVetDTO = this.vetService.create(vetDTO);
+        log.info("" + vetDTO);
+
+        // ------------ Delete ---------------
+
+        try {
+            this.vetService.delete(newVetDTO.getId());
+        } catch (VetNotFoundException e) {
+            fail(e.getMessage());
+        }
+
+        // ------------ Validation ---------------
+
+        try {
+            this.vetService.findById(newVetDTO.getId());
+            assertTrue(false);
+        } catch (VetNotFoundException e) {
+            assertTrue(true);
+        }
+    }
+
 }
