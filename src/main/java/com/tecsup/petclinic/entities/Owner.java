ca979efa13710519933455a27142f40c06a8e66a
Jilder Dionisio <jilder.dionisio@tecsup.edu.pe>
2026-05-26 14:33:15 -0500

    test: agregar pruebas de creación y búsqueda de dueños

---
+++ src/main/java/com/tecsup/petclinic/entities/Owner.java
@@ -0,0 +1,25 @@
+package com.tecsup.petclinic.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity(name = "owners")
+@NoArgsConstructor
+@Data
+@AllArgsConstructor
+public class Owner {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+    private String firstName;
+    private String lastName;
+    private String address;
+    private String city;
+    private String telephone;
+}
--- src/test/java/com/tecsup/petclinic/services/OwnerServiceTest.java
+++ src/test/java/com/tecsup/petclinic/services/OwnerServiceTest.java
@@ -1,37 +1,84 @@
 package com.tecsup.petclinic.services;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
+
+import com.tecsup.petclinic.dtos.OwnerDTO;
+import com.tecsup.petclinic.exceptions.OwnerNotFoundException;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
 
 @SpringBootTest
 @Slf4j
 public class OwnerServiceTest {
 
-	//@Autowired
-   //	private OwnerService ownerService;
+    @Autowired
+    private OwnerService ownerService;
 
-	/**
-	 * 
-	 */
-	/*
-	@Test
-	public void testFindOwnerById() {
+    @Test
+    public void testFindOwnerById() {
 
-		long ID = 1;
-		String NAME = "Jaime";
-		Owner owner = null;
-		
-		try {
-			
-			owner = ownerService.findById(ID);
-			
-		} catch (OwnertNotFoundException e) {
-			fail(e.getMessage());
-		}
-		log.info("" + owner);
+        final String FIRST_NAME_EXPECTED = "George";
+        final String LAST_NAME_EXPECTED = "Franklin";
 
-		assertEquals(NAME, owner.getName());
+        Integer ID = 1;
 
-	}
-	*/
+        OwnerDTO owner = null;
+
+        try {
+            owner = this.ownerService.findById(ID);
+        } catch (OwnerNotFoundException e) {
+            fail(e.getMessage());
+        }
+
+        assertEquals(FIRST_NAME_EXPECTED, owner.getFirstName());
+        assertEquals(LAST_NAME_EXPECTED, owner.getLastName());
+    }
+
+    @Test
+    public void testFindOwnerByLastName() {
+
+        String FIND_LAST_NAME = "Davis";
+        int SIZE_EXPECTED = 2;
+
+        List<OwnerDTO> owners = this.ownerService.findByLastName(FIND_LAST_NAME);
+
+        assertEquals(SIZE_EXPECTED, owners.size());
+    }
+
+    @Test
+    public void testCreateOwner() {
+
+        String FIRST_NAME = "Luis";
+        String LAST_NAME = "Garcia";
+        String ADDRESS = "123 Main St.";
+        String CITY = "Lima";
+        String TELEPHONE = "999888777";
+
+        OwnerDTO ownerDTO = OwnerDTO.builder()
+                .firstName(FIRST_NAME)
+                .lastName(LAST_NAME)
+                .address(ADDRESS)
+                .city(CITY)
+                .telephone(TELEPHONE)
+                .build();
+
+        OwnerDTO newOwnerDTO = this.ownerService.create(ownerDTO);
+
+        log.info("OWNER CREATED :" + newOwnerDTO.toString());
+
+        assertNotNull(newOwnerDTO.getId());
+        assertEquals(FIRST_NAME, newOwnerDTO.getFirstName());
+        assertEquals(LAST_NAME, newOwnerDTO.getLastName());
+        assertEquals(ADDRESS, newOwnerDTO.getAddress());
+        assertEquals(CITY, newOwnerDTO.getCity());
+        assertEquals(TELEPHONE, newOwnerDTO.getTelephone());
+    }
+
 }
